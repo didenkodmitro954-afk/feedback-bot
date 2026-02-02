70b536378437fab12cf159dcd8e2ec9ff07f3772
didenkodmitro954-afk <didenkodmitro954@gmail.com>
2026-02-02 19:21:56 +0200

    Update database.py

--- database.py
+++ database.py
@@ -17,20 +17,6 @@ CREATE TABLE IF NOT EXISTS admins (
 )
 """)
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS giveaways (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT
-)
-""")
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS giveaway_users (
-    user_id INTEGER,
-    giveaway_id INTEGER
-)
-""")
-
 conn.commit()
 
 # ---------------- Функції ----------------
@@ -40,25 +26,3 @@ def add_user(user_id, username):
     conn.commit()
 
 def add_admin(admin_id):
-    cursor.execute("INSERT OR IGNORE INTO admins (id) VALUES (?)", (admin_id,))
-    conn.commit()
-
-def remove_admin(admin_id):
-    cursor.execute("DELETE FROM admins WHERE id=?", (admin_id,))
-    conn.commit()
-
-def get_all_admins():
-    cursor.execute("SELECT id FROM admins")
-    return [x[0] for x in cursor.fetchall()]
-
-def create_giveaway(title):
-    cursor.execute("INSERT INTO giveaways (title) VALUES (?)", (title,))
-    conn.commit()
-
-def join_giveaway(user_id, giveaway_id):
-    cursor.execute("INSERT INTO giveaway_users (user_id, giveaway_id) VALUES (?, ?)", (user_id, giveaway_id))
-    conn.commit()
-
-def get_giveaways():
-    cursor.execute("SELECT * FROM giveaways")
-    return cursor.fetchall()

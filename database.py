7f606693a090383fde52a906645a55db50dda2c6
didenkodmitro954-afk <didenkodmitro954@gmail.com>
2026-02-02 19:25:32 +0200

    Update database.py

--- database.py
+++ database.py
@@ -22,7 +22,26 @@ conn.commit()
 # ---------------- Функції ----------------
 
 def add_user(user_id, username):
-    cursor.execute("INSERT OR IGNORE INTO users (id, username) VALUES (?, ?)", (user_id, username))
+    cursor.execute(
+        "INSERT OR IGNORE INTO users (id, username) VALUES (?, ?)",
+        (user_id, username)
+    )
     conn.commit()
 
 def add_admin(admin_id):
+    cursor.execute(
+        "INSERT OR IGNORE INTO admins (id) VALUES (?)",
+        (admin_id,)
+    )
+    conn.commit()
+
+def remove_admin(admin_id):
+    cursor.execute(
+        "DELETE FROM admins WHERE id=?",
+        (admin_id,)
+    )
+    conn.commit()
+
+def get_all_admins():
+    cursor.execute("SELECT id FROM admins")
+    return [x[0] for x in cursor.fetchall()]

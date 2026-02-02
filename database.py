3368aec2b80d6b4b009c2eee86a2ac17b0979825
didenkodmitro954-afk <didenkodmitro954@gmail.com>
2026-02-02 19:07:27 +0200

    Update database.py

--- database.py
+++ database.py
@@ -24,30 +24,16 @@ CREATE TABLE IF NOT EXISTS giveaways (
 )
 """)
 
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS giveaway_users (
+    user_id INTEGER,
+    giveaway_id INTEGER
+)
+""")
+
 conn.commit()
 
 # ---------------- Функції ----------------
 
 def add_user(user_id, username):
-    cursor.execute("INSERT OR IGNORE INTO users (id, username) VALUES (?, ?)", (user_id, username))
-    conn.commit()
-
-def add_admin(admin_id):
-    cursor.execute("INSERT OR IGNORE INTO admins (id) VALUES (?)", (admin_id,))
-    conn.commit()
-
-def remove_admin(admin_id):
-    cursor.execute("DELETE FROM admins WHERE id=?", (admin_id,))
-    conn.commit()
-
-def get_all_admins():
-    cursor.execute("SELECT id FROM admins")
-    return [x[0] for x in cursor.fetchall()]
-
-def create_giveaway(title):
-    cursor.execute("INSERT INTO giveaways (title) VALUES (?)", (title,))
-    conn.commit()
-
-def get_giveaways():
-    cursor.execute("SELECT * FROM giveaways")
-    return cursor.fetchall()
+    cursor.execute("INSERT OR IGNORE INTO users (id, username) VALUES (?, ?

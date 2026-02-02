225a6ec6924c1556226e8b05079e985f53d8ab9e
didenkodmitro954-afk <didenkodmitro954@gmail.com>
2026-02-02 19:04:17 +0200

    Update database.py

--- database.py
+++ database.py
@@ -7,8 +7,7 @@ cursor = conn.cursor()
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY,
-    username TEXT,
-    blocked INTEGER DEFAULT 0
+    username TEXT
 )
 """)
 
@@ -25,13 +24,6 @@ CREATE TABLE IF NOT EXISTS giveaways (
 )
 """)
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS giveaway_users (
-    user_id INTEGER,
-    giveaway_id INTEGER
-)
-""")
-
 conn.commit()
 
 # ---------------- Функції ----------------
@@ -39,23 +31,6 @@ conn.commit()
 def add_user(user_id, username):
     cursor.execute("INSERT OR IGNORE INTO users (id, username) VALUES (?, ?)", (user_id, username))
     conn.commit()
-
-def is_blocked(user_id):
-    cursor.execute("SELECT blocked FROM users WHERE id=?", (user_id,))
-    r = cursor.fetchone()
-    return r and r[0] == 1
-
-def block_user(user_id):
-    cursor.execute("UPDATE users SET blocked=1 WHERE id=?", (user_id,))
-    conn.commit()
-
-def unblock_user(user_id):
-    cursor.execute("UPDATE users SET blocked=0 WHERE id=?", (user_id,))
-    conn.commit()
-
-def get_all_users():
-    cursor.execute("SELECT id FROM users WHERE blocked=0")
-    return [x[0] for x in cursor.fetchall()]
 
 def add_admin(admin_id):
     cursor.execute("INSERT OR IGNORE INTO admins (id) VALUES (?)", (admin_id,))
@@ -71,10 +46,6 @@ def get_all_admins():
 
 def create_giveaway(title):
     cursor.execute("INSERT INTO giveaways (title) VALUES (?)", (title,))
-    conn.commit()
-
-def join_giveaway(user_id, giveaway_id):
-    cursor.execute("INSERT INTO giveaway_users (user_id, giveaway_id) VALUES (?, ?)", (user_id, giveaway_id))
     conn.commit()
 
 def get_giveaways():

694b3a9dfb15445f7e5681ef6e92f9600b2d0e6c
didenkodmitro954-afk <didenkodmitro954@gmail.com>
2026-02-03 08:23:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import random
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 
-# ================= НАСТРОЙКИ =================
-TOKEN = "8511337609:AAFNtvQWoD4rhyYugouVgsspw0FKorm-rDM"
+# =======8511337609:AAFNtvQWoD4rhyYugouVgsspw0FKorm-rDM========== НАСТРОЙКИ =================
+TOKEN = ""
 OWNER_USERNAME = "userveesna"
 
 logging.basicConfig(level=logging.INFO)
